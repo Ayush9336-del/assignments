a14129318df1a331ba59956a3d934a9f3e191654
ayush <Ayush.Gaur@tothenew.com>
2026-02-02 14:32:19 +0530

    Updated code

---
+++ script.js
@@ -0,0 +1,156 @@
+let amt = prompt("Enter the amount ") ; 
+let interest = prompt("enter interest rate ") ; 
+let time = prompt("enter the number of years") ; 
+
+let ans = (amt * interest * time ) / 100 ; 
+
+console.log(ans) ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let obj1 = {
+//      b : 2 , 
+//      c : 3 , 
+//      a :1  , 
+//      d : 4 
+// }
+// obj1.item = 324234; 
+
+// obj1['another'] = 2234234  ;
+// console.log(Object.keys(obj1)) ; 
+
+ 
+
+// for ( var key in obj1) {
+//     console.log("key = " + key + " , value = " + obj1[key]) ; 
+// }
+
+// let obj2= {
+//      '2' : 'b', 
+//      '3' :'c' , 
+//      '1'  : 'a' ,
+//      '4' : 'd'
+//  }
+
+// console.log(Object.keys(obj2)) ; 
+
+
+
+// for ( var key in obj2) {
+//     console.log("key = " + key + " , value = " + obj2[key]) ; 
+// }
+// let obj3= {
+//      b : 2 , 
+//      c : 3 , 
+//      a :1  , 
+//      d : 4 , 
+//      '2' : 'b', 
+//      '3' :'c' , 
+//      '1'  : 'a' ,
+//      '4' : 'd'
+//  }
+
+// console.log(Object.keys(obj3)) ; 
+
+
+
+// for ( var key in obj3) {
+//     console.log("key = " + key + " , value = " + obj3[key]) ; 
+// }
+// let obj4 = {
+//      'b2' : 2 , 
+//      'c3' : 3 , 
+//      'a1' : 1 , 
+//      'd4' : 4 , 
+//     '2b' : 'b', 
+//     '3c' :'c' , 
+//     '1a' : 'a' ,
+//     '4d' : 'd'
+//  }
+
+// console.log(Object.keys(obj4)) ; 
+
+// for ( var key in obj4) {
+//     console.log("key = " + key + " , value = " + obj4[key]) ; 
+// }
+
+
+
+// var x = {
+//      x1  : 123 
+// } ; 
+
+// var y = {
+//     // y2 : 456 
+// }
+
+// var temp = { 
+//     a:1 , 
+//     b :2 
+// }
+
+// temp[x] = 111 
+// temp[y] = 222 
+// // temp['x'] = 111 
+// // temp['y'] = 222 
+
+// console.log(temp[x]  + " " + temp[y])
+
+
+//  let arr = [ 2,3,5,34,54,34,3,276,97,98 ,  undefined] ; 
+//  arr['20'] = 324 ; 
+
+
+//  for ( let i in arr ) 
+//     console.log(arr[i]) ; 
+
+
+// arr.forEach((e)=>{
+//     console.log(e)
+// })
